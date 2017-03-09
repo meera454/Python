40d8b43d38a4a42d0d0b355cc3334fa0491eda9b
meera454 <meera454@hotmail.com>
2017-03-09 11:32:19 +0530

    Assignment5- Geocoding API with a Database and  Visualizing data
    Google Geocoding API with a Database and 
    Visualizing data on Google MapIn

---
+++ Coursera/Using_Databases_with_Python/geodata/geodump.py
@@ -0,0 +1,40 @@
+# Question >> https://pr4e.dr-chuck.com/tsugi/mod/peer-grade/index.php?PHPSESSID=312dc17c63b545740b50b92222854110
+import sqlite3
+import json
+import codecs
+
+conn = sqlite3.connect('geodata.sqlite')
+cur = conn.cursor()
+
+cur.execute('SELECT * FROM Locations')
+fhand = codecs.open('where.js','w', "utf-8")
+fhand.write("myData = [\n")
+count = 0
+for row in cur :
+    data = str(row[1])
+    try: js = json.loads(str(data))
+    except: continue
+
+    if not('status' in js and js['status'] == 'OK') : continue
+
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    if lat == 0 or lng == 0 : continue
+    where = js['results'][0]['formatted_address']
+    where = where.replace("'","")
+    try :
+        print where, lat, lng
+
+        count = count + 1
+        if count > 1 : fhand.write(",\n")
+        output = "["+str(lat)+","+str(lng)+", '"+where+"']"
+        fhand.write(output)
+    except:
+        continue
+
+fhand.write("\n];\n")
+cur.close()
+fhand.close()
+print count, "records written to where.js"
+print "Open where.html to view the data in a browser"
+
